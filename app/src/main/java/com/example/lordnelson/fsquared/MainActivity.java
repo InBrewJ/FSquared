826270980cfa9bf2526efee9381567161559efcc
InBrewJ <jason.brewer101@gmail.com>
2017-02-18 18:34:21 +0000

    Very simple message. Sendio!

--- app/src/main/java/com/example/lordnelson/fsquared/MainActivity.java
+++ app/src/main/java/com/example/lordnelson/fsquared/MainActivity.java
@@ -2,13 +2,26 @@ package com.example.lordnelson.fsquared;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    public final static String EXTRA_MESSAGE = "com.example.lordnelson.fsquared.MESSAGE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        int a = 0;
+
+    }
+
+    /** Called when the user clicks the Send button */
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
