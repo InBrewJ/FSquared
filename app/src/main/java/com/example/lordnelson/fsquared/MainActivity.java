b241a3c5b2dacd8d640d449fd806276f8288d810
InBrewJ <jason.brewer101@gmail.com>
2017-02-18 17:16:40 +0000

    First commit

---
+++ app/src/main/java/com/example/lordnelson/fsquared/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.lordnelson.fsquared;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
